1768a95ea0fa8126b10932a3dbded953f55cdf99
Shubham Gupta <shubham.gupta2956@gmail.com>
2024-07-28 20:00:10 +0530

    update homepage

--- composeApp/src/commonMain/kotlin/Home.kt
+++ composeApp/src/commonMain/kotlin/Home.kt
@@ -1,20 +1,58 @@
-import androidx.compose.runtime.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.semantics.Role.Companion.Button
 
 @Composable
-@Preview
 fun HomePage() {
     var isPlaying by remember { mutableStateOf(false) }
 
-     return Scaffold (backgroundColor = Color.Black) {
-        Column {
-            Text(" Home Page", color = Color.White)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.TopCenter)
+                .fillMaxWidth()
+        ) {
+            // Header Text
+            BasicText(
+                text = "Home Page",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+        
         }
+
     }
+}
+
+
+@Composable
+fun PreviewHomePage() {
+    HomePage()
 }
