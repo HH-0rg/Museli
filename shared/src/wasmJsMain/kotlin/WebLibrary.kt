8ec773e1ea82da1353694f040b95bbd8ad105a64
sin3point14 <manaschaudhary2000@gmail.com>
2024-08-13 21:02:04 +0530

    Add reference for WebLibrary

--- shared/src/wasmJsMain/kotlin/WebLibrary.kt
+++ shared/src/wasmJsMain/kotlin/WebLibrary.kt
@@ -7,6 +7,7 @@ class WebLibrary(private val rootUrl: String): CoroutineScope by CoroutineScope(
     Library() {
     override lateinit var songsList: List<String>
     override lateinit var playlists: Map<String, List<String>>
+    // https://www.reddit.com/r/Kotlin/comments/lx99rv/comment/gpmmsr9/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     val initialization: Job = launch {
         songsList = getSongsRemote(rootUrl).map { filePath ->
             val normalizedPath = filePath.replace("\\", "/")  // Normalize backslashes to forward slashes
