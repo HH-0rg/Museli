2950856631d9ff77033bda99ae7435766332bd58
sin3point14 <manaschaudhary2000@gmail.com>
2024-08-13 14:01:10 +0530

    Add checks to DesktopLibrary

--- shared/src/jvmMain/kotlin/Music.jvm.kt
+++ shared/src/jvmMain/kotlin/Music.jvm.kt
@@ -1,36 +1,30 @@
 import java.io.File
 
-var SongExtensions = listOf("mp3", "wav", "flac", "opus", "m4a")
+val SongExtensions = listOf("mp3", "wav", "flac", "opus", "m4a")
 
 fun getSongs(rootDir: String): List<String> {
-    val safeRootDir = rootDir ?: return emptyList() // Handle null case if necessary
-
-    val directory = File(safeRootDir)
-    return if (directory.isDirectory) {
-        directory.listFiles { file -> file.isFile && file.extension in SongExtensions}
-            ?.map { it.absolutePath } ?: emptyList()
-    } else {
-        emptyList()
-    }
+    val directory = File(rootDir)
+    check(directory.isDirectory) { "$rootDir is not a directory" }
+    return directory.listFiles { file -> file.isFile && file.extension in SongExtensions}
+        ?.map { it.absolutePath } ?: emptyList()
 }
 
 fun getPlaylists(rootDir: String): Map<String, List<String>> {
-    val rootDirFile = File(rootDir);
-    val result = mutableMapOf<String, MutableList<String>>()
-
-    val directories = rootDirFile.listFiles { file -> file.isDirectory } ?: emptyArray()
-
-    for (dir in directories) {
+    val directory = File(rootDir);
+    check(directory.isDirectory) { "$rootDir is not a directory" }
+    val playlists = mutableMapOf<String, MutableList<String>>()
+    val directories = directory.listFiles { file -> file.isDirectory } ?: emptyArray()
+    for (subDir in directories) {
         val musicFiles = mutableListOf<String>()
 
-        val files = dir.listFiles { file -> file.isFile && file.extension in SongExtensions }
+        val files = subDir.listFiles { file -> file.isFile && file.extension in SongExtensions }
             ?: emptyArray()
 
         for (file in files) {
             musicFiles.add(file.absolutePath)
         }
 
-        result[dir.name] = musicFiles
+        playlists[subDir.name] = musicFiles
     }
-    return result
+    return playlists
 }
