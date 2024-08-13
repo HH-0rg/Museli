23c3e8db1047d2bcd9136efb78ce6654b3b6da80
sin3point14 <manaschaudhary2000@gmail.com>
2024-08-13 19:22:43 +0530

    Make mediaPlayer non nullable, remove listener from DesktopController

--- shared/src/jvmMain/kotlin/DesktopController.kt
+++ shared/src/jvmMain/kotlin/DesktopController.kt
@@ -5,76 +5,69 @@ import uk.co.caprica.vlcj.player.component.AudioPlayerComponent
 
 
 class DesktopController: MediaPlayerController {
-    private var mediaPlayer: MediaPlayer? = null
-    private var listener: MediaPlayerListener? = null
+    private var mediaPlayer: MediaPlayer
 
-    private fun initMediaPlayer() {
+    init {
         NativeDiscovery().discover()
-
         AudioPlayerComponent().also { mediaPlayer = it.mediaPlayer() }
-
-        mediaPlayer?.events()?.addMediaPlayerEventListener(object : MediaPlayerEventAdapter() {
-            override fun mediaPlayerReady(mediaPlayer: MediaPlayer?) {
-                super.mediaPlayerReady(mediaPlayer)
-                listener?.onReady()
-            }
-
-            override fun finished(mediaPlayer: MediaPlayer?) {
-                super.finished(mediaPlayer)
-                listener?.onAudioCompleted()
-            }
-
-            override fun error(mediaPlayer: MediaPlayer?) {
-                super.error(mediaPlayer)
-                listener?.onError()
-            }
-        })
     }
 
     override fun prepare(
         songUri: String, listener: MediaPlayerListener
     ) {
-        if (mediaPlayer == null) {
-            initMediaPlayer()
-            this.listener = listener
+        mediaPlayer.events()?.addMediaPlayerEventListener(object : MediaPlayerEventAdapter() {
+            override fun mediaPlayerReady(mediaPlayer: MediaPlayer?) {
+                super.mediaPlayerReady(mediaPlayer)
+                listener.onReady()
+            }
+
+            override fun finished(mediaPlayer: MediaPlayer?) {
+                super.finished(mediaPlayer)
+                listener.onAudioCompleted()
+            }
+
+            override fun error(mediaPlayer: MediaPlayer?) {
+                super.error(mediaPlayer)
+                listener.onError()
+            }
+        })
+
+        if (mediaPlayer.status()?.isPlaying == true) {
+            mediaPlayer.controls()?.stop()
         }
 
-        if (mediaPlayer?.status()?.isPlaying == true) {
-            mediaPlayer?.controls()?.stop()
-        }
-
-        mediaPlayer?.media()?.play(songUri)
+        mediaPlayer.media()?.play(songUri)
     }
 
     override fun start() {
-        mediaPlayer?.controls()?.start()
+        mediaPlayer.controls()?.start()
     }
 
     override fun pause() {
-        mediaPlayer?.controls()?.pause()
+        mediaPlayer.controls()?.pause()
     }
 
     override fun stop() {
-        mediaPlayer?.controls()?.stop()
+        mediaPlayer.controls()?.stop()
     }
 
     override fun isPlaying(): Boolean {
-        return mediaPlayer?.status()?.isPlaying ?: false
+        return mediaPlayer.status()?.isPlaying ?: false
     }
 
     override fun seek(): Long? {
-        return mediaPlayer?.status()?.time()
+        return mediaPlayer.status()?.time()
     }
 
     override fun mediaDuration(): Long? {
-        return mediaPlayer?.status()?.length()
+        return mediaPlayer.status()?.length()
     }
 
     override fun setTime(time: Long) {
-        mediaPlayer?.controls()?.setTime(time)
+        mediaPlayer.controls()?.setTime(time)
     }
     
     override fun release() {
-        mediaPlayer?.release()
+        mediaPlayer.release()
     }
 }
